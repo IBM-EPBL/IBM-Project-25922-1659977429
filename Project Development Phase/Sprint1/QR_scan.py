1b2a6320ec48d608ac08f499b750870585ad2cd0
Prithivi Maruthachalam <prithivimaruthachalam@gmail.com>
2022-11-24 15:37:17 +0530

    added project development files

---
+++ Project Development Phase/Sprint1/QR_scan.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np 
+import time
+import pyzbar.pyzbar as pyzbar 
+from ibmcloudant.cloudant_v1 import CloudantV1 
+from ibmcloudant import CouchDbSessionAuthenticator 
+from ibm_cloud_sdk_core.authenticators import BasicAuthenticator
+
+
+authenticator = BasicAuthenticator ('apikey-v2-sometestkey', '82bca5b0a21ab96b35d79d5651c15947')
+service = CloudantV1(authenticator=authenticator)
+service.set_service_url ('service url')
+
+cap= cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+
+while True:
+	_, frame = cap.read ()
+	decodedObjects = pyzbar. decode (frame)
+	for obj in decodedObjects:
+		#print ("Data", obj.data) 
+		a=obj.data.decode('UTF-8') 
+		cv2.putText(frame,"Ticket", (50, 50), font, 2,(255,0, 0), 3)
+		#print (a)
+		try:
+			response = service.get_document(db= 'booking', doc_id = a).get_result()
+			print(response)
+			time.sleep (5)
+		except Exception as e:
+			print ("Not a Valid Ticket")
+			time.sleep (5)
+	cv2.imshow ("Frame", frame)
+	if cv2.waitKey (1) & 0xFF == ord ('q'):
+		break
+
+cap.release ()
+cv2.destroyAllWindows ()
+client.disconnect ()
